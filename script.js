a1a14b08cae4a7af4d8640a7413b273995358d2a
rydei <rishukamboj7166@gmail.com>
2026-09-17 20:25:36 +0530

    WEB OS1 FINISHED!!!! (i hope so) I have added two new apps : timer and nasa ctrl panel. Also the sidebar and icons do work now!!!

--- script.js
+++ script.js
@@ -6,10 +6,7 @@ var timeHolder = document.querySelector("#time");
 timeHolder.innerHTML = timeRN;}
 
 setInterval(TimeUpdater,1000);
-
-//Draggable Windows
-dragElement(document.getElementById("outlinemain"))
-dragElement(document.getElementById("nasa"))
+/*dragElement(document.getElementById("snote"))*/
 
 function dragElement(element) {
 
@@ -18,8 +15,8 @@ function dragElement(element) {
     var currentX = 0;
     var currentY = 0;
 
-    if (document.getElementById(element.Id + "header")) {
-        document.getElementById(element.Id + "header").onmousedown = startDragging;
+    if (document.getElementById(element.id + "header")) {
+        document.getElementById(element.id + "header").onmousedown = startDragging;
     } else{
         element.onmousedown = startDragging;
     }
@@ -53,6 +50,7 @@ function dragElement(element) {
 
 //Resize or Close
 //1. info
+
 var outlineScreen = document.querySelector("#outlinemain")
 var outlineClose = document.querySelector("#outlineclose")
 var outlineOpen = document.querySelector("#outlineopen")
@@ -75,7 +73,9 @@ outlineClose.addEventListener("click", function() {
 outlineOpen.addEventListener("click", function() {
   openWindow(outlineScreen);
 });
+
 //2. nasa
+
 var nasaScreen = document.querySelector("#nasa");
 var nasaClose = document.querySelector("#nasaclose");
 var nasaOpen = document.querySelector("#nasaopen");
@@ -86,6 +86,33 @@ nasaClose.addEventListener("click", function() {
 
 nasaOpen.addEventListener("click", function() {
   openWindow(nasaScreen);
+});
+
+/*3.SNote
+
+var noteScreen = document.querySelector("#note");
+var noteClose = document.querySelector("#noteclose");
+var noteOpen = document.querySelector("#noteopen");
+
+noteClose.addEventListener("click", function() {
+  closeWindow(noteScreen);
+});
+noteOpen.addEventListener("click", function() {
+  openWindow(noteScreen);
+});*/
+
+//4. timer
+
+var timerScreen = document.querySelector("#timer");
+var timerClose = document.querySelector("#timerclose");
+var timerOpen = document.querySelector("#timeropen");
+
+timerClose.addEventListener("click", function() {
+  closeWindow(timerScreen);
+});
+
+timerOpen.addEventListener("click", function() {
+  openWindow(timerScreen);
 });
 
 
@@ -137,3 +164,94 @@ function initializeWindow(elementName) {
 
 initializeWindow("nasa")
 initializeWindow("outlinemain")
+initializeWindow("timer")
+/*initializeWindow("snote")*/
+
+let defaultTime = 300;
+let timeLeft = defaultTime; 
+let timerInterval = null;
+
+const display = document.getElementById('display');
+const startB = document.getElementById('startB');
+const pauseB = document.getElementById('pauseB');
+const resetB = document.getElementById('resetB');
+const minutes = document.getElementById('minutes');
+const setTimeB = document.getElementById('setTimeB');
+
+function updateDisplay() {
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+  display.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  }
+
+function setCustomTime() {
+  const userMinutes = parseInt(minutes.value);
+  if (isNaN(userMinutes) || userMinutes <= 0) {
+    alert("Please enter a valid number of minutes");
+    return;
+    }
+
+  pauseTimer(); 
+  defaultTime = userMinutes * 60; 
+  timeLeft = defaultTime;
+  updateDisplay();
+}
+
+function startTimer() {
+if (timerInterval !== null) return; 
+timerInterval = setInterval(() => {
+  if (timeLeft > 0) {
+    timeLeft--;
+    updateDisplay();
+  } else {
+    clearInterval(timerInterval);
+    timerInterval = null;
+    alert("Time is up!");
+  }}, 1000); 
+}
+
+function pauseTimer() {
+  clearInterval(timerInterval);
+  timerInterval = null;
+}
+
+function resetTimer() {
+  pauseTimer();
+  timeLeft = defaultTime; 
+  updateDisplay();
+}
+
+setTimeB.addEventListener('click', setCustomTime);
+startB.addEventListener('click', startTimer);
+pauseB.addEventListener('click', pauseTimer);
+resetB.addEventListener('click', resetTimer);
+updateDisplay();
+
+var sidebar = document.querySelector("#sidebar");
+var logoB = document.querySelector("#logo");
+
+logoB.onclick = function toggleSidebar(){
+  if (sidebar.style.display === "flex") {
+    sidebar.style.display = "none";
+    logoB.classList.remove("selected");
+  } else{
+    sidebar.style.display = "flex";
+    logoB.classList.add("selected");
+  }
+};
+function toggleApp(IconId,windowId) {
+
+  var icon = document.getElementById(IconId);
+  var screen = document.getElementById(windowId);
+
+  if (screen.style.display === "flex") {
+    screen.style.display = "none";
+    icon.classList.remove("selected");
+  } else{
+    screen.style.display = "flex";
+    icon.classList.add("selected");
+  };
+};
+
+document.getElementById("nasaapp").onclick = function() { toggleApp("nasaapp","nasa")};
+document.getElementById("timerapp").onclick = function(){ toggleApp("timerapp","timer")};
